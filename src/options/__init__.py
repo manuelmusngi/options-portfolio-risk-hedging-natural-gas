4165d881906c7fc700738504937c574cdc4a7b94
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2026-07-16 15:46:41 -0400

    Delete src/options directory

--- src/options/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Option instrument models: ShortPut, LongPut, ShortCall."""
-from .short_put import ShortPut
-from .long_put import LongPut
-from .short_call import ShortCall
-
-__all__ = ["ShortPut", "LongPut", "ShortCall"]
